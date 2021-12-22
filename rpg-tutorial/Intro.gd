dba8906e1c9651079978277b54eb2262d6c3d619
avivilloz <avivilloz@gmail.com>
2021-12-22 14:58:45 +0200

    ur

--- rpg-tutorial/Intro.gd
+++ rpg-tutorial/Intro.gd
@@ -1,8 +1,11 @@
 extends Control
 
 const WorldResource = preload("res://World.tscn")
+const ButtonSoundResource = preload("res://ButtonSound.tscn")
 
 func _on_Button_pressed():
+	var ButtonSoundScene = ButtonSoundResource.instance()
+	add_child(ButtonSoundScene)
 	var worldScene = WorldResource.instance()
 	get_parent().add_child(worldScene)
 	queue_free()
