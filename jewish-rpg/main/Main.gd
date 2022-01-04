245f488efbcbc987ca4ec0e93b578c6899b93bd7
avivilloz <avivilloz@gmail.com>
2022-01-04 22:49:22 +0200

    ur

--- jewish-rpg/main/Main.gd
+++ jewish-rpg/main/Main.gd
@@ -1,16 +1,11 @@
 extends Control
 
+onready var m_srcWorldScene = preload("res://world/World.tscn")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Button_pressed():
+	var nodeWorldScene = m_srcWorldScene.instance()
+	get_parent().add_child(nodeWorldScene)
+	queue_free()
